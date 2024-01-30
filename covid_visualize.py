7a4993b25f7514cdb65b6867ebf667c16f568568
whitebaby007 <150632425+whitebaby007@users.noreply.github.com>
2024-01-30 05:24:45 +0100

    Update covid_visualize.py

--- covid_visualize.py
+++ covid_visualize.py
@@ -3,6 +3,7 @@ import matplotlib
 # matplotlib.use('TkAgg') # Mac specific
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+import matplotlib.patches as mpatches
 
 
 class Visualization:
@@ -16,12 +17,19 @@ class Visualization:
         self.state_grid = np.full((self.h, self.w), -1)
 
         # Define a custom colormap including a color for 'empty' state
-        self.cmap = ListedColormap(['black', 'white', 'red', 'yellow', 'green'])  # Adding 'green' for 'empty'
+        self.cmap = ListedColormap(['black', 'white', 'red', 'yellow', 'blue'])  # Adding 'blue' for 'immune'
 
         # Initialize the plot with the image grid
         self.fig, self.ax = plt.subplots()
         self.im = self.ax.imshow(self.image_grid, cmap='gray', vmin=0, vmax=1)
-        self.overlay = self.ax.imshow(self.state_grid, cmap=self.cmap, vmin=-1, vmax=3, alpha=0.5)
+        self.overlay = self.ax.imshow(self.state_grid, cmap=self.cmap, vmin=-1, vmax=4, alpha=0.5)  # vmax updated to 4
+        self.legend_patches = [
+            mpatches.Patch(color='red', label='Infected'),
+            mpatches.Patch(color='yellow', label='Susceptible'),
+            mpatches.Patch(color='blue', label='Immune'),
+            mpatches.Patch(color='black', label='Empty/Background'),  # Assuming black is empty
+            # Add more patches if there are more states
+        ]
 
     def update(self, t, movingHumanPopulation, humanPopulation):
         # Reset the state grid to -1 (empty) each time
@@ -32,15 +40,19 @@ class Visualization:
             if m.state == 'I':  # Infected
                 self.state_grid[m.position[1], m.position[0]] = 1
             elif m.state == 'S':  # Susceptible
-                self.state_grid[m.position[1], m.position[0]] = 3
+                self.state_grid[m.position[1], m.position[0]] = 2
+            elif m.state == 'M':  # Immune
+                self.state_grid[m.position[1], m.position[0]] = 3  # New code for immune state
 
         for h in humanPopulation:
             if h.state == 'I':  # Infected
                 self.state_grid[h.position[1], h.position[0]] = 1
             elif h.state == 'S':  # Susceptible
+                self.state_grid[h.position[1], h.position[0]] = 2
+            elif h.state == 'M':  # Immune
                 self.state_grid[h.position[1], h.position[0]] = 3
 
-        # Update the overlay with the state grid
+        self.ax.legend(handles=self.legend_patches, loc='upper right')  # Adjust loc as needed
         self.overlay.set_data(self.state_grid)
         self.ax.set_title('t = %i' % t)
         plt.draw()
